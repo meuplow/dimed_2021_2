e8a523705dcc8dfc5c85332e89536710eaaee510
meuplow <gabrielgm@ufcspa.edu.br>
2021-09-01 16:04:14 -0300

    routing

--- demoforms/src/app/app-routing.module.ts
+++ demoforms/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavegacaoComponent } from './navegacao/navegacao.component';
+import { PaginanaoencontradaComponent } from './paginanaoencontrada/paginanaoencontrada.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'navegacao', component: NavegacaoComponent },
+  { path: '**', component: PaginanaoencontradaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
