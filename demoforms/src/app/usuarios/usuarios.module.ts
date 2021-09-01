3a4457568d676346eb809f6eb881933e77835ba6
meuplow <gabrielgm@ufcspa.edu.br>
2021-09-01 16:09:25 -0300

    criacao componentes usuario-cadastro e usuario-lista

--- demoforms/src/app/usuarios/usuarios.module.ts
+++ demoforms/src/app/usuarios/usuarios.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsuariosRoutingModule } from './usuarios-routing.module';
+import { UsuariosCadastroComponent } from './usuarios-cadastro/usuarios-cadastro.component';
+import { UsuariosListaComponent } from './usuarios-lista/usuarios-lista.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsuariosCadastroComponent,
+    UsuariosListaComponent
+  ],
   imports: [
     CommonModule,
     UsuariosRoutingModule
