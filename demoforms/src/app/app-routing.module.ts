62137e7cbc32226bad9d0b1379475afbed74deae
meuplow <gabrielgm@ufcspa.edu.br>
2021-09-01 17:05:14 -0300

    ajuste de rotas e views

--- demoforms/src/app/app-routing.module.ts
+++ demoforms/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoasvindasComponent } from './boasvindas/boasvindas.component';
 import { NavegacaoComponent } from './navegacao/navegacao.component';
 import { PaginanaoencontradaComponent } from './paginanaoencontrada/paginanaoencontrada.component';
 
 const routes: Routes = [
-  { path: 'navegacao', component: NavegacaoComponent },
+  { path: '', component: BoasvindasComponent },
   { path: '**', component: PaginanaoencontradaComponent}
 ];
 
